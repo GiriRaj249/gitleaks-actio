3e7efdd8fd3042da5ca45ec2507ffd47075a47eb
GiriRaj249 <80571820+GiriRaj249@users.noreply.github.com>
2022-07-08 16:37:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,8 @@ then
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then 
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF... > commit_list.txt
-  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
-  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG)
+  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --commits-file=commit_list.txt $CONFIG
+  CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --commits-file=commit_list.txt $CONFIG)
 fi
 
 if [ $? -eq 1 ]
